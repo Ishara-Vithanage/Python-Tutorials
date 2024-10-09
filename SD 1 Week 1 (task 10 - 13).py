74382e59d73c1be3254d9dd7477a2b935991c7f5
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-09 22:54:31 +0530

    task 10, 11 completed

---
+++ SD 1 Week 1 (task 10 - 13).py
@@ -0,0 +1,19 @@
+print("This is task 10")
+# Temperature Conversion
+temp = int(input("Enter temperature in Celsius: ")) # Get the input temperature
+fa_value = (temp*9/5)+32 # Convert Celsius value to Farenhite
+print(f"Farenhite value: {fa_value}")
+
+
+print("-"*10)
+
+
+print("This is task 11")
+# Grocery bill estimator
+# Get user inputs
+item_1 = float(input("Enter value of item 1: "))
+item_2 = float(input("Enter value of item 2: "))
+item_3 = float(input("Enter value of item 3: "))
+# Output sum value
+sum = item_1 + item_2 + item_3
+print(f"Total value is {sum}")

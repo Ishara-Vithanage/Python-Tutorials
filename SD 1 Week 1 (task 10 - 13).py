03e62488d7f4db2bdbd972fd27d0e3bcf4e7e6b0
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-09 23:06:49 +0530

    all tasks comleted

--- SD 1 Week 1 (task 10 - 13).py
+++ SD 1 Week 1 (task 10 - 13).py
@@ -17,3 +17,30 @@ item_3 = float(input("Enter value of item 3: "))
 # Output sum value
 sum = item_1 + item_2 + item_3
 print(f"Total value is {sum}")
+
+
+print("-"*10)
+
+
+print("This is task 12")
+#Distance converter
+distance_m = int(input("Enter distance in meter: ")) # Get input distance
+# Convert it into kilometers and to miles
+distance_km = distance_m/1000
+distance_mil = distance_km/1.60934
+# Print the output
+print(f"Distance in kilometers and miles aocordingly {distance_km}, {distance_mil}")
+
+
+print("-"*10)
+
+
+print("This is task 13")
+# BMI Calculator
+# Get user weight and height as inputs
+weight = float(input("Enter weight: "))
+height = float(input("Enter height: "))
+# Calculate BMI value
+bmi = weight/(height**2)
+# Print the output
+print(f"Your BMI is {bmi}")

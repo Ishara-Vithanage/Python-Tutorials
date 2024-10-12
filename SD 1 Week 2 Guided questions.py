eaf9097ffbe74ab2df5856b59cdac4c116a8cbc2
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-12 22:26:45 +0530

    guided questions

---
+++ SD 1 Week 2 Guided questions.py
@@ -0,0 +1,50 @@
+print("-"*30)
+print("1st question")
+# This is 1st question
+
+#Area of a circle calculator
+rad = int(input("Enter radius: "))# Get radius as input
+area = 22/7*(rad)**2 # Calculate area
+print(f"Area is {area}") # Return area value
+
+
+
+print("-"*30)
+print("2nd question")
+# This is 2nd question
+
+# Get salary input and percentage
+sal = int(input("Enter the salary: "))
+percent = int(input("Enter percentage: "))
+after_calc = sal * (100 + percent) / 100
+print(f"New salary is {after_calc}")
+
+
+
+print("-"*30)
+print("3rd question")
+# This is 3rd question
+
+# Get the volume of a cylinder
+rad = int(input("Enter radius: ")) # Get inputs
+height = int(input("Enter height: "))
+# Calculation part
+vol = 22/7*(rad)**2*height
+print(f"Volume is {vol}")
+
+
+
+print("-"*30)
+print("4th question")
+# This is 4th question
+
+# Get inputs
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+height = int(input("Enter height: "))
+
+area = 2*length*width + 2*length*height + 2*width*height # Calculation
+print(f"Area is {area}") # Outputs
+
+
+

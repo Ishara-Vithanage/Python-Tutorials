182b5f9cc783983ab1751d6a6da11b8e984ebe02
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-12 21:59:51 +0530

    Week 2 guided first 5 tasks

---
+++ SD 1 Week 2 (task 1-5).py
@@ -0,0 +1,83 @@
+print("1st question")
+# This is 1st question
+
+task_1 = 8+2*5 # task 1
+print(task_1)
+task_2 = (8+2)*5 # task 2
+print(task_2)
+task_3 = 20/4 # task 3
+print(task_3)
+task_4 = 20//3 # task 4
+print(task_4)
+task_5 = 20%3 # task 5
+print(task_5)
+task_6 = 2**3 # task 6
+print(task_6)
+
+
+
+print("-"*10)
+print("2nd question")
+# This is 2nd question
+
+a = 10+5 # Assign variable a
+print(a)
+b = 30 # Assign variable b
+print(b)
+b += 5 # add 5 using compound assignment operator
+print(b)
+
+
+
+print("-"*10)
+print("3rd question")
+# This is 3rd question
+
+x = 10 # Given value
+x += 5 # Add 5
+print(x)
+x *= 3 # Multiply 3
+print(x)
+x -= 2 # Subtract 2
+print(x)
+x /= 4 # Divide 4
+print(x)
+
+
+
+print("-"*10)
+print("4th question")
+# This is 4th question
+
+# Identifying the data types of variables
+num = 42
+print(type(num))
+num = 3.14
+print(type(num))
+bool = True
+print(type(bool))
+greet = "Hello World"
+print(type(greet))
+
+
+
+print("-"*10)
+print("5th question")
+# This is 5th question
+
+# task 1
+print("task_1")
+word = "123"
+nt_word = int(word) # convert it to int
+print(nt_word + 10) # add 5 and output the value
+
+# task 2
+print("task_2")
+fruit = str(5) # Converts given number into a string
+print(fruit+ " apples") # Concatenate
+
+#task 3
+print("task_3")
+num = 3.9 # Given number
+nw_num = int(num) # Convert to an int
+print(nw_num)

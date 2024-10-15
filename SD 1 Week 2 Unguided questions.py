8ea0615384ad7e94356b07d977ba7b0d09099f77
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-15 05:39:51 +0530

    task 5 and 6 done

---
+++ SD 1 Week 2 Unguided questions.py
@@ -0,0 +1,48 @@
+print(task 5)
+# This is task 5
+# Grade calculator
+
+# Get marks as inputs
+mark_1 = int(input"Enter the marks of 1st subject: ")
+mark_2 = int(input"Enter the marks of 2nd subject: ")
+mark_3 = int(input"Enter the marks of 3rd subject: ")
+mark_4 = int(input"Enter the marks of 4th subject: ")
+mark_5 = int(input"Enter the marks of 5th subject: ")
+
+# Calculations
+avg = (mark_1 + mark_2 + mark_3 + mark_4 + mark_5)/5
+print(avg)
+
+# Decide grade
+if 100 => avg => 35:
+    if avg => 75:
+        print("Distinction")
+    else:
+        print("Pass")
+else:
+    print("Fail")
+    
+
+
+
+print("-"*10)
+print("task 6")
+# This is task 6
+# Savings Goal Tracker
+
+# Get user inputs
+target_amt = int(input("Enter target saving amount: "))
+current_amt = int(input("Enter current amount: "))
+monthly_amt = int(input("Enter monthly saving amount: "))
+
+# Calculations
+remain_months = (target_amt - current_amt)/monthly_amt
+print(f"You need to save {remain_months} more month/s to achieve the target saving.")
+
+
+
+print("-"*10)
+print("task 7")
+# This is taks 7
+# Distance between two points
+

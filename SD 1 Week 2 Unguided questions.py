fac39a581f8cf40b7215363c0af0f953f8eeb255
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-10-15 22:14:15 +0530

    unguided tasks completed

--- SD 1 Week 2 Unguided questions.py
+++ SD 1 Week 2 Unguided questions.py
@@ -1,21 +1,23 @@
-print(task 5)
+import math
+
+print("task 5")
 # This is task 5
 # Grade calculator
 
 # Get marks as inputs
-mark_1 = int(input"Enter the marks of 1st subject: ")
-mark_2 = int(input"Enter the marks of 2nd subject: ")
-mark_3 = int(input"Enter the marks of 3rd subject: ")
-mark_4 = int(input"Enter the marks of 4th subject: ")
-mark_5 = int(input"Enter the marks of 5th subject: ")
+mark_1 = int(input("Enter the marks of 1st subject: "))
+mark_2 = int(input("Enter the marks of 2nd subject: "))
+mark_3 = int(input("Enter the marks of 3rd subject: "))
+mark_4 = int(input("Enter the marks of 4th subject: "))
+mark_5 = int(input("Enter the marks of 4th subject: "))
 
 # Calculations
 avg = (mark_1 + mark_2 + mark_3 + mark_4 + mark_5)/5
 print(avg)
 
 # Decide grade
-if 100 => avg => 35:
-    if avg => 75:
+if 100 >= avg > 35:
+    if avg >= 75:
         print("Distinction")
     else:
         print("Pass")
@@ -45,4 +47,16 @@ print("-"*10)
 print("task 7")
 # This is taks 7
 # Distance between two points
+
+# Get user inputs
+x1 = int(input("Enter x value of first point: "))
+y1 = int(input("Enter y value of first point: "))
+x2 = int(input("Enter x value of second point: "))
+y2 = int(input("Enter y value of second point: "))
+
+# Calculation
+dist = math.sqrt((x1-x2)**2+(y1-y2)**2)
+
+# Display output
+print(f"Distance is {dist}")
 
